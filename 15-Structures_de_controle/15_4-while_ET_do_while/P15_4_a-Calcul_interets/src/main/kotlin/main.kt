4e681bfb5f36ad9cf6038843cf33f910dfb6e248
KLNPRO <killianemmanuel68@gmail.com>
2024-09-10 11:07:28 +0200

    exo 15.4.a

--- 15-Structures_de_controle/15_4-while_ET_do_while/P15_4_a-Calcul_interets/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_4-while_ET_do_while/P15_4_a-Calcul_interets/src/main/kotlin/main.kt
@@ -10,7 +10,10 @@ fun findYears(depo: Double): Int{
     val max = 700000
     var years = 0
     //
-    // implement the while loop here
+    while (deposit<=max){
+        deposit=deposit*1.075
+        years++
+    }
     //
     return years
 }
